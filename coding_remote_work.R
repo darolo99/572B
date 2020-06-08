4780655fbcc7d4313a65903feccfbd37aeded48e
Daniel <daniel.rojas.lozano@gmail.com>
2020-06-08 14:24:29 -0700

    loading data from ACS

--- coding_remote_work.R
+++ coding_remote_work.R
@@ -1,4 +1,5 @@
 library(ipumsr)
 data_file <- "usa18.dat"
 ddi_data <- "DDI.xml"
-ddi <- read_ipums_ddi(ddi_data)
+ddi <- read_ipums_ddi(ddi_data) # metadata here
+acs_df <- read_ipums_micro(ddi_data, data_file = data_file)
