4f7e4b0840ff4ffab33057e0710cf40ac1f1c742
darolo99 <59588600+darolo99@users.noreply.github.com>
2020-06-29 22:23:12 -0700

    Update coding_remote_work.R

--- coding_remote_work.R
+++ coding_remote_work.R
@@ -4,9 +4,10 @@ ddi_data <- "DDI.xml"
 ddi <- read_ipums_ddi(ddi_data) # metadata here
 acs_df <- read_ipums_micro(ddi_data, data_file = data_file)
 #####
-acs_df = readRDS("acs_df.rds") 
 
-# Collapsing data at the county level ($countyfip)
+
+# Collapsing data at the county level ($PWCOUNTY)
 library(data.table)
+acs_df = readRDS("acs_df_2.rds") 
 acs_df = as.data.table(acs_df)
-county_level = setDT(acs_df)[, lapply(.SD, mean), by = .(MULTYEAR, COUNTYFIP)]
+county_level = setDT(acs_df)[, lapply(.SD, mean), by = .(MULTYEAR, PWCOUNTY)]
